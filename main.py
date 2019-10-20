78f55156052dad542f32767caea68258c06f2668
VegetarianRacoon <igovinda100@gmail.com>
2019-10-20 21:28:23 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+TUGAS BESAR PENGENALAN KOMPUTASI
+Dosen: Dr. Rahadian Yusuf, S.T.,M.T.
+SIMULASI APLIKASI GOJEK
+
+I Gede Govindabhakta        16519020
+Ryandito Diandaru           16519150
+Kevin Angga Yumawan         16519210
+Aindrea Rayhan Supriyanto   16519360
+'''
+
