db78305057407914cd67df70c655f8f2d79279df
VegetarianRacoon <igovinda100@gmail.com>
2019-10-21 11:11:06 +0700

    Fungsi placeholder

--- main.py
+++ main.py
@@ -9,3 +9,30 @@ Kevin Angga Yumawan         16519210
 Aindrea Rayhan Supriyanto   16519360
 '''
 
+
+
+###FUNGSI PLACEHOLDER
+def DistFind(asal, destinasi):
+    return 25
+
+def RestaurantFind(i):
+    tempat = ["Crisbar", "Salman", "Upnormal"]
+    return tempat[i]
+
+def BayarGopay(gopay, harga):
+    #Parameter gopay adalah variabel jumlah gopay yang ada
+    #Parameter harga adalah variabel harga barang yang dibeli
+    #Nanti BayarGopay() returnnya jumlah yang dibayar pakai gopay dan jumlah yang pakai cash
+    payWithCash = 0
+    payWithGopay = float(harga)
+    return payWithGopay, payWithCash
+###END OF FUNGSI PLACEHOLDER
+
+
+
+###MAIN APPLICATION
+def Gojek():
+    #Semua fungsi nanti dipakai disini, didefinisiinnya di atas
+    #gopay = int(0)
+    return
+###END OF MAIN APPLICATION
