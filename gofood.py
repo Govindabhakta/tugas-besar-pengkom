490c5bae11bb31486a622d3a3fbd2cf867cc7a10
VegetarianRacoon <igovinda100@gmail.com>
2019-10-21 11:23:42 +0700

    Update gofood.py

--- gofood.py
+++ gofood.py
@@ -9,11 +9,9 @@ menu = [[16000, 20500, 14000, 13000],
         [16000, 13000, 16000, 17500]]
 
 
-'''
-Bikin dia print nama restoran dulu, baru menunya
-Pakai placeholder RestaurantFind() yang ada di main.py
-'''
 
+###Bikin dia print nama restoran dulu, baru menunya
+###Pakai placeholder RestaurantFind() yang ada di main.py
 for i in range (3):
     for j in range (4):
         print(menu[i][j],'', end='')
@@ -32,9 +30,8 @@ while selesai == False:
     if done == 0:
         selesai = True
 
-'''
-Buat cari jarak, pakai placeholder yang ada di main.py
-'''
+
+###Buat cari jarak, pakai placeholder yang ada di main.py
 if merchant == 1:
     jarak = 5000
 elif merchant == 2:
@@ -50,9 +47,8 @@ bayar = int(input('pilih metode pembayaran (cash=0, gopay=1) : '))
 if bayar == 0:
     print('pesanan anda sedang diproses...')
 elif bayar == 1:
-    '''
-    Buar bayar pakai gopay, pakai placeholder BayarGopay() yang di main.py
-    '''
+    
+    ###Buat bayar pakai gopay, pakai placeholder BayarGopay() yang di main.py
     print('saldo gopay anda adalah ',saldo)
     if saldo >= total:
         cukup = True
