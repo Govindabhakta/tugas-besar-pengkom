281793c9c88f019354c80a09f11d3945e78d0d34
VegetarianRacoon <igovinda100@gmail.com>
2019-10-25 15:13:26 +0700

    Update BayarGoPay.py

--- BayarGoPay.py
+++ BayarGoPay.py
@@ -6,16 +6,19 @@ harga = variabel yang didapatkan dari gofood atau goTransport (integer)
 asumsi user tidak salah input
 '''
 def BayarGopay(gopay, harga):
+    #Inisialisasi variable
     payWithGopay = 0
     payWithCash = 0
     
     if gopay >= harga:
+        #Gopay cukup untuk membayar
         payWithGopay = harga
         print("Pay ",payWithGopay," with Gopay, ")
         print("Bayar?")
         print("(jawaban (Y) untuk bayar dan (N) untuk tidak bayar)")
         Bayar = input()
     elif gopay < harga:
+        #Gopay tidak cukup untuk membayar
         payWithCash = harga - gopay
         payWithGopay = gopay
         print("Pay ",payWithGopay," with Gopay + ", payWithCash," with Cash")
@@ -23,7 +26,7 @@ def BayarGopay(gopay, harga):
         print("(jawaban (Y) untuk bayar dan (N) untuk tidak bayar)")
         Bayar = input()
     
-    if Bayar == Y:
+    if Bayar == "Y":
         if gopay >= harga:
             gopay = gopay - harga
         elif gopay < harga:
